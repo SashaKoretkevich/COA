7a75e7ae8cf980e9a9c2c20f597c285c0c40c0ff
SashaKoretkevich <126869113+SashaKoretkevich@users.noreply.github.com>
2025-03-06 23:09:08 +0300

    Add files via upload

---
+++ UserService/app/models.py
@@ -0,0 +1,64 @@
+from pydantic import BaseModel, EmailStr, Field, field_validator
+from typing import Optional
+import re
+
+class userCreate(BaseModel):
+    firstName: str = Field(..., min_length=3, max_length=30, description="Имя, от 3 до 30 символов")
+    secondName: str = Field(..., min_length=3, max_length=30, description="Фамилия, от 3 до 30 символов")
+    userName: str = Field(..., min_length=3, max_length=20, description="Ник, от 3 до 20 символов")
+    mail: EmailStr = Field(..., description="Электронная почта")
+    password: str = Field(..., min_length=10, max_length=20, description="Пароль, от 10 до 20 знаков")
+    age: int
+    gender: str
+    status: str = Field(..., min_length=3, max_length=30, description="Статус, от 3 до 30 знаков")
+    phoneNumber: str = Field(..., description="Номер телефона в международном формате, начинающийся с '+'")
+
+    @field_validator('age')
+    def check_age(cls, value):
+        if value < 14:
+            raise ValueError('Возраст должен быть больше 14 лет')
+        return value
+    @field_validator('phoneNumber')
+    def validate_phone_number(cls, value: str) -> str:
+        if not re.match(r'^\+\d{11}$', value):
+            raise ValueError('Номер телефона должен начинаться с "+" и содержать 11 цифр')
+        return value
+    @field_validator('gender')
+    def validate_gender(cls, value: str) -> str:
+        if not value == "Male" and not value == "Female":
+            raise ValueError('Нкпрвыилллвтава')
+        return value
+
+class userLogin(BaseModel):
+    userName: Optional[str] = Field(None, min_length=3, max_length=20, description="Ник, от 3 до 20 символов")
+    mail: Optional[EmailStr] = Field(None, description="Электронная почта")
+    password: str = Field(..., min_length=10, max_length=20, description="Пароль, от 10 до 20 знаков")
+
+class tokenSend(BaseModel):
+    token: str
+
+class userUpdate(BaseModel):
+    firstName: str = Field(..., min_length=3, max_length=30, description="Имя, от 3 до 30 символов")
+    secondName: str = Field(..., min_length=3, max_length=30, description="Фамилия, от 3 до 30 символов")
+    age: int
+    gender: str
+    status: str = Field(..., min_length=3, max_length=30, description="Статус, от 3 до 30 знаков")
+    phoneNumber: str = Field(..., description="Номер телефона в международном формате, начинающийся с '+'")
+    token: str
+    
+    @field_validator('age')
+    def check_age(cls, value):
+        if value < 14:
+            raise ValueError('Возраст должен быть больше 14 лет')
+        return value
+    @field_validator('phoneNumber')
+    def validate_phone_number(cls, value: str) -> str:
+        if not re.match(r'^\+\d{11}$', value):
+            raise ValueError('Номер телефона должен начинаться с "+" и содержать 11 цифр')
+        return value
+    @field_validator('gender')
+    def validate_gender(cls, value: str) -> str:
+        if not value == "Male" and not value == "Female":
+            raise ValueError('Неправильно указан пол')
+        return value
+
